3d003d3312508902332a9d4aee3e74b272b5133c
Sajjad Paracha <sajadparacha@gmail.com>
2023-11-18 07:49:21 +0300

    Create DDL For MYSQL.enable only when you want to create the DDL.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,21 +1,21 @@
 insert into APPLICATION (application_id,appl_description,appl_name) values (3,'DIOR','Drilling Reports');
-Insert into  COMMON_PROBLEM_AND_SOLUTION (common_problem_id,problem_desc,Application_application_id) values (3,'Problem',3);
-insert into SOLUTION  (solution_id,seq_number,sol_description,common_problem_common_problem_id) values (3,1,'solution',3);
+Insert into  COMMON_PROBLEM_AND_SOLUTION (common_problem_id,problem_desc,Application_application_id) values (3,'Problem 1',3);
+insert into SOLUTION  (solution_id,seq_number,sol_description,common_problem_common_problem_id) values (3,1,'solution 1',3);
 
 insert into APPLICATION (application_id,appl_description,appl_name) values (4,'EWellFile','Well File Attachment');
-Insert into  COMMON_PROBLEM_AND_SOLUTION (common_problem_id,problem_desc,Application_application_id) values (4,'Problem',4);
-insert into SOLUTION  (solution_id,seq_number,sol_description,common_problem_common_problem_id) values (4,1,'solution',4);
+Insert into  COMMON_PROBLEM_AND_SOLUTION (common_problem_id,problem_desc,Application_application_id) values (4,'Problem 2',4);
+insert into SOLUTION  (solution_id,seq_number,sol_description,common_problem_common_problem_id) values (4,1,'solution 2',4);
 
 insert into APPLICATION (application_id,appl_description,appl_name) values (1,'CSAS','Cement Slurry Application');
-Insert into  COMMON_PROBLEM_AND_SOLUTION (common_problem_id,problem_desc,Application_application_id) values (1,'Problem',1);
-insert into SOLUTION  (solution_id,seq_number,sol_description,common_problem_common_problem_id) values (1,1,'solution',1);
+Insert into  COMMON_PROBLEM_AND_SOLUTION (common_problem_id,problem_desc,Application_application_id) values (1,'Problem 3',1);
+insert into SOLUTION  (solution_id,seq_number,sol_description,common_problem_common_problem_id) values (1,1,'solution 3',1);
 
 insert into APPLICATION (application_id,appl_description,appl_name) values (2,'BRAVO','Drilling Best Practices');
-Insert into  COMMON_PROBLEM_AND_SOLUTION (common_problem_id,problem_desc,Application_application_id) values (2,'Problem',2);
-insert into SOLUTION  (solution_id,seq_number,sol_description,common_problem_common_problem_id) values (2,1,'solution',2);
+Insert into  COMMON_PROBLEM_AND_SOLUTION (common_problem_id,problem_desc,Application_application_id) values (2,'Problem 4',2);
+insert into SOLUTION  (solution_id,seq_number,sol_description,common_problem_common_problem_id) values (2,1,'solution 4',2);
 
 insert into APPLICATION (application_id,appl_description,appl_name) values (5,'SADK','SADK');
 -- noinspection SqlDialectInspection
 
-Insert into  COMMON_PROBLEM_AND_SOLUTION (common_problem_id,problem_desc,Application_application_id) values (5,'Problem',5)
-    insert into SOLUTION  (solution_id,seq_number,sol_description,common_problem_common_problem_id) values (5,1,'solution',5);
+Insert into  COMMON_PROBLEM_AND_SOLUTION (common_problem_id,problem_desc,Application_application_id) values (5,'Problem 5',5)
+    insert into SOLUTION  (solution_id,seq_number,sol_description,common_problem_common_problem_id) values (5,1,'solution 5',5);
